6b38760ed4d1b7c127570652d1f622ec4980143f
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-01-05 11:59:14 +0530

    file system lib

--- index.js
+++ index.js
@@ -20,5 +20,25 @@ const arr = (__dirname.split(path.sep))
 console.log(arr)
 
 
+// FILE SYSTEM:
+
+const fs = require('fs');
+
+fs.readFile('textfile.txt', 'utf-8', (err, data) => {
+    if (err) throw err
+    console.log(data)
+});
+console.log('reading completed');
 
 
+const a = fs.readFileSync('textfile.txt')
+console.log(a.toString());
+console.log('reading completed');
+
+
+fs.writeFile('textfilewrite.txt', "this is text file write", () => {
+    console.log('file sync written');
+});
+
+d = fs.writeFileSync('textfilewrite.txt', "this is writeSync");
+console.log("file written")
