315e6174c256e343a2304d1b259b0033baef301a
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-01-05 11:22:25 +0530

    path lib

--- index.js
+++ index.js
@@ -2,7 +2,23 @@
 // console.log("hello from second console")
 // console.log("hello from 3rd console")
 
-const BookDetail = require('./second')
+// const BookDetail = require('./second')
 
-console.log(BookDetail)
+// console.log(BookDetail)
+
+
+//PATH:
+const path = require('path')
+
+console.log("dirname is", path.dirname('second.js'));
+console.log("dirname is", path.dirname(__filename));
+console.log("ext is:",path.extname('second.js'));
+console.log("ext is:",path.extname(__filename));
+console.log("parse: ",path.parse(__filename));
+
+const arr = (__dirname.split(path.sep))
+console.log(arr)
+
+
+
 
