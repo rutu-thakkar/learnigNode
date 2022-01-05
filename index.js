5dc54ba2e9f187908c804a13432be05336a18d5e
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-01-05 10:30:27 +0530

    require and export basics

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-console.log("hello world!")
-console.log("hello from second console")
-console.log("hello from 3rd console")
+// console.log("hello world!")
+// console.log("hello from second console")
+// console.log("hello from 3rd console")
+
+const BookDetail = require('./second')
+
+console.log(BookDetail)
+
---
+++ second.js
@@ -0,0 +1,7 @@
+bookDetail = {
+    bookName: 'lord of the Rings',
+    authorName : 'abc',
+    isPurchased: true
+}
+// console.log(bookDetail)
+module.exports = bookDetail;
