870a2c1bc83f3506a2f9940e8dd6922279487fba
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-01-05 16:23:06 +0530

    express app

--- expressapp.js
+++ expressapp.js
@@ -1,15 +1,28 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
 const app = express()
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'public')))
+
+
 app.get('/', (req,res) => {
-    res.send("hello world!")
+    // res.send("hello world!")
+    // res.sendFile(path.join(__dirname, 'demosite.html'))
+    // res.json({
+        // rutu: "hello rutu"
+    // })
 });
 
+app.get('/home/:name', (req,res) => {
+    // console.log("hello " + req.params.name);
+    res.send("hello "+ req.params.name)
+})
+
 app.get('/about', (req,res) => {
-    res.send("Hey There!, Know more about us")
+    res.send("Hey There!, Know more about us!!")
 });
 
 app.listen(port, () => {
